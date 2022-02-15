f49a41ab738bae7d9870d53853a3ba2d9ec1c053
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2022-02-15 18:19:18 +0600

    m suite

---
+++ src/test/java/com/in28minutes/junit/helper/AllTests.java
@@ -0,0 +1,12 @@
+package com.in28minutes.junit.helper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ArraysCompareTest.class, QuickBeforeAfterTest.class, StringHelperParameterizedTest.class,
+		StringHelperTest.class })
+public class AllTests {
+
+}
