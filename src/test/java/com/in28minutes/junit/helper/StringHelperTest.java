31da848de3348a2df5057c2a2be96d5ad085b124
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2022-02-15 17:45:58 +0600

    Commit assertion, before-after, parameterized

--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -5,10 +5,27 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringHelperTest {
+	
+	StringHelper helper = new StringHelper();
 
 	@Test
-	public void test() {
-		
+	public void testTruncateAInFirst2Positions_C1() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions_C2() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
+	
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_NegativeScenario() {
+		assertFalse(helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+	}
+	
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_PositiveScenario() {
+		assertTrue(helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
 	}
 
 }
